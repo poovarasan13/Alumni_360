6fa62cbac74deb5b80894ddaf5721076604fb6b8
Poovarasan <poovarasan936161@gmail.com>
2025-03-12 09:11:44 +0530

    updated contoller

--- controller/student.controller.js
+++ controller/student.controller.js
@@ -1,26 +1,29 @@
 const Students=require('../modal/Students.js');
 
-const login= async(req,res)=>{
-      console.log("login details");
-       const {rollno,password}=req.body;
-      try{
-        const studentData= await Students.findOne({rollno})
-         if(studentData.password==password)
-         {
-            console.log(studentData);
-            return res.status(200).json({success:true, message:"Login Successfully", name:studentData.name,mobile:studentData.mobile});
-         }
-         else{
-            return res.status(404).json({success:false,message:"Incorrect Password"});
-         }
-      }
-      catch(err)
-      {
-       return res.status(400).json({message:err.message});
-      }
+const login = async (req, res) => {
+   console.log("Login attempt received");
+   const { rollno, password } = req.body;
+   try {
+       const studentData = await Students.findOne({ rollno });
+       if (!studentData) {
+           return res.status(404).json({ success: false, message: "Student not found" });
+       }
 
-}
+       if (studentData.password === password) {
+           console.log(studentData);
+           return res.status(200).json({ 
+               success: true, 
+               message: "Login Successfully", 
+               name: studentData.name, 
+               mobile: studentData.mobile 
+           });
+       } else {
+           return res.status(401).json({ success: false, message: "Incorrect Password" });
+       }
+   } catch (err) {
+       return res.status(500).json({ success: false, message: err.message });
+   }
+};
 
-module.exports=login;
-
+module.exports = login;
 
