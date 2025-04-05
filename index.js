aac301cc8cce6384e32299fd536de00e4c4730aa
poovarasan13 <poovarasan936161@gmail.com>
2025-04-05 12:12:21 +0530

    updated forum

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-
-const express=require('express');
+const express = require('express');
 const connectDB = require('./lib/db');
-const StudentRouter=require('./router/student.router.js');
-const webinarRoutes=require('./router/webinarRoutes.js');
-const postRoutes=require('./router/postRoutes.js');
-const internshipRoutes=require('./router/internshipRoutes.js');
-const app=express();
-const port=9000;
-const cors=require('cors');
+const StudentRouter = require('./router/student.router.js');
+const webinarRoutes = require('./router/webinarRoutes.js');
+const postRoutes = require('./router/postRoutes.js');
+const internshipRoutes = require('./router/internshipRoutes.js');
+const forum = require('./router/forum.js');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 9000;
+
 connectDB();
-app.use(express.json());
 app.use(cors());
-app.use('/',StudentRouter);
+app.use(express.json());
+
+// Serve static files from 'uploads' folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Routes
+app.use('/', StudentRouter);
+app.use('/forums', forum); // FIXED route
 app.use("/webinars", webinarRoutes);
-app.use("/posts",postRoutes);
+app.use("/posts", postRoutes);
 app.use("/internships", internshipRoutes);
-app.listen(port,()=>{
-    console.log("Server is running on port = ",port);
-})
+
+app.listen(port, () => {
+  console.log("Server is running on port =", port);
+});
